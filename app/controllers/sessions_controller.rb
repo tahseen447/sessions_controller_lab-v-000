4339f0f902f730c6ed146c7c7cd0b80f2b747891
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2019-01-30 22:48:40 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     if !params[:name]
-      redirect_to '/login'
+      redirect_to controller: 'sessions', action: 'new'
       else
         session[:name] = params[:name]
         redirect_to controller: 'application', action: 'hello'
